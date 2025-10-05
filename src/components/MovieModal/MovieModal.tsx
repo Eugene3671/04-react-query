d4bb3af7c1c8d6609ef424037d3155873c3244ad
Eugene <korzh367@gmail.com>
2025-10-05 17:23:27 +0200

    done

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,7 +1,7 @@
 import { createPortal } from "react-dom";
 import css from "./MovieModal.module.css";
-import type Movie from "../../types/movie";
 import { useEffect } from "react";
+import type { Movie } from "../../types/movie";
 
 interface MovieModalProps {
   movie: Movie;
